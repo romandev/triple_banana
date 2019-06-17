6c9d8d5c45b7b67229e2a7f01dc8727bd92b1838
Jinho Bang <zino@chromium.org>
2019-06-17 22:20:11 +0900

    Introduce TripleBananaApplication class (#5)
    The TripleBananaApplication extends ChromeApplication. We can use the
    class as an entry point for application.
    
    Refs: #4

---
+++ browser/java/org/triple/banana/TripleBananaApplication.java
@@ -0,0 +1,14 @@
+// Copyright 2019 The Triple Banana Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.triple.banana;
+
+import org.chromium.chrome.browser.ChromeApplication;
+
+public class TripleBananaApplication extends ChromeApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
