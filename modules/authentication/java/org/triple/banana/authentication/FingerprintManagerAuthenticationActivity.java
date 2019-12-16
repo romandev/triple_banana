8bdd70a7b4dca154afccbc908f16c8c136f0243c
Jdragon <34815234+jdragonbae@users.noreply.github.com>
2019-12-16 13:40:42 +0900

    Add fingerprint authentication ui (#71)
    This patch does three things:
    - Implementation for fingerprint authentication. The UI is implemented
      in dialog style.
    - Do cancel handler.postdelayed process when FINGERPRINT_ERROR_LOCKOUT.
    - Grouping authentication java source for fixing unused lint error.
    
    Refs: #61, #95, #106, #115, #116

--- modules/authentication/java/org/triple/banana/authentication/FingerprintManagerAuthenticationActivity.java
+++ modules/authentication/java/org/triple/banana/authentication/FingerprintManagerAuthenticationActivity.java
@@ -4,15 +4,32 @@
 
 package org.triple.banana.authentication;
 
+import static android.hardware.fingerprint.FingerprintManager.FINGERPRINT_ERROR_LOCKOUT;
+import static android.hardware.fingerprint.FingerprintManager.FINGERPRINT_ERROR_LOCKOUT_PERMANENT;
+
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
 import android.support.v4.os.CancellationSignal;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.triple.banana.R;
 
 @TargetApi(Build.VERSION_CODES.M)
 public class FingerprintManagerAuthenticationActivity extends TranslucentActivity {
+    private static final long ERROR_TIMEOUT_MILLIS = 1600;
+    private final Handler mHandler = new Handler();
+
     private FingerprintManagerCompat mFingerprintManager;
+    private Button mCancelButton;
+    private TextView mAuthenticationDescriptionText;
+    private CancellationSignal mCancellationSignal;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,14 +39,30 @@ public class FingerprintManagerAuthenticationActivity extends TranslucentActivit
             return;
         }
 
+        // Setting for finguerprint ui
+        setContentView(R.layout.fingerprint_dialog);
+        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+        layoutParams.gravity = Gravity.BOTTOM;
+        getWindow().setAttributes(layoutParams);
+        setFinishOnTouchOutside(false);
+
+        mAuthenticationDescriptionText = findViewById(R.id.authentication_description_text);
+        mCancelButton = findViewById(R.id.cancel_button);
+        mCancelButton.setOnClickListener((View v) -> { handleResult(false); });
         mFingerprintManager = FingerprintManagerCompat.from(this);
 
         if (isFingerprintAuthAvailable()) {
-            CancellationSignal cancellationSignal = new CancellationSignal();
+            mCancellationSignal = new CancellationSignal();
             // TODO(#54): Inject cryptoObject for using private key
             mFingerprintManager.authenticate(
-                    null, 0, cancellationSignal, new AuthenticationCallback(), null);
+                    null, 0, mCancellationSignal, new AuthenticationCallback(), null);
         }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        handleResult(false);
     }
 
     private boolean isFingerprintAuthAvailable() {
@@ -38,6 +71,10 @@ public class FingerprintManagerAuthenticationActivity extends TranslucentActivit
     }
 
     private void handleResult(boolean result) {
+        if (mCancellationSignal != null) {
+            mCancellationSignal.cancel();
+            mCancellationSignal = null;
+        }
         AuthenticationManagerImpl.handleResult(result);
         finish();
     }
@@ -45,11 +82,19 @@ public class FingerprintManagerAuthenticationActivity extends TranslucentActivit
     private class AuthenticationCallback extends FingerprintManagerCompat.AuthenticationCallback {
         @Override
         public void onAuthenticationError(int errorCode, CharSequence errorString) {
-            handleResult(false);
+            if (errorCode == FINGERPRINT_ERROR_LOCKOUT_PERMANENT) {
+                showLockout(getResources().getString(R.string.authentication_lockout_permenent));
+            } else if (errorCode == FINGERPRINT_ERROR_LOCKOUT) {
+                showLockout(getResources().getString(R.string.authentication_lockout));
+            } else {
+                showError(errorString);
+            }
         }
 
         @Override
-        public void onAuthenticationHelp(int helpCode, CharSequence helpString) {}
+        public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
+            showError(helpString);
+        }
 
         @Override
         public void onAuthenticationSucceeded(
@@ -59,7 +104,30 @@ public class FingerprintManagerAuthenticationActivity extends TranslucentActivit
 
         @Override
         public void onAuthenticationFailed() {
-            handleResult(false);
+            showError(getResources().getString(R.string.authentication_not_recognized));
+        }
+
+        private void showError(CharSequence errorString) {
+            mHandler.removeCallbacksAndMessages(null);
+            mAuthenticationDescriptionText.setText(errorString);
+            mAuthenticationDescriptionText.setTextColor(
+                    getResources().getColor(R.color.authentication_warning_color));
+            mHandler.postDelayed(() -> {
+                mAuthenticationDescriptionText.setText(
+                        getResources().getString(R.string.authentication_description));
+                mAuthenticationDescriptionText.setTextColor(
+                        getResources().getColor(R.color.authentication_hint_color));
+            }, ERROR_TIMEOUT_MILLIS);
+        }
+
+        private void showLockout(CharSequence lockoutString) {
+            mHandler.removeCallbacksAndMessages(null);
+            mAuthenticationDescriptionText.setText(lockoutString);
+            mAuthenticationDescriptionText.setTextColor(
+                    getResources().getColor(R.color.authentication_warning_color));
+            mAuthenticationDescriptionText.setGravity(Gravity.CENTER);
+            mAuthenticationDescriptionText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+            mCancelButton.setText(R.string.authentication_check);
         }
     }
 }
