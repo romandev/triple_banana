2fe7e9f8476eee032fe93db40dbfe37c4f7011cb
Jaeyong Bae <jdragon.bae@gmail.com>
2020-01-24 18:09:31 +0900

    Add api for toolbar items (#172)
    This patch means to declare api for toolbar items.
    After this patch, each api should be implemented.
    
    Refs: #171

---
+++ public/org/triple/banana/public_api/export/BananaToolsApi.java
@@ -0,0 +1,29 @@
+// Copyright 2020 The Triple Banana Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.triple.banana.public_api.export;
+
+public enum BananaToolsApi {
+    instance;
+
+    public void back() {}
+    public void forward() {}
+    public void share() {}
+    public void search() {}
+    public void addNewTab() {}
+    public void addBookmark() {}
+    public void goBookmark() {}
+    public void addBlock() {}
+    public void changeSecretMode() {}
+    public void addSecretTab() {}
+    public void download() {}
+    public void changeDesktopMode() {}
+    public void findInpage() {}
+    public void addHomeScreen() {}
+    public void reload() {}
+    public void goVisitHistory() {}
+    public void print() {}
+    public void goPasswordSetting() {}
+    public void goArchive() {}
+}
