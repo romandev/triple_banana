4962e2a7c3e6f40865c3b1baa0dbc5b13ecfa22a
Jinho Bang <zino@chromium.org>
2020-01-28 21:56:02 +0900

    Fix a bug that the web page is sometimes freezing (#189)
    After rebase `m79`, there is a bug that the web page is sometimes
    freezing. It occurs because the pending receiver is passed to wrong path
    at OnBindHostReceiverForRenderer (which is to handle a request of
    binding coming from renderer).
    To resolve this issue, we only handle the receivers that are explicitly
    allowed.
    
    Refs: #188

--- modules/public/interfaces.h
+++ modules/public/interfaces.h
@@ -5,16 +5,57 @@
 #ifndef TRIPLE_BANANA_MODULES_PUBLIC_INTERFACES_H_
 #define TRIPLE_BANANA_MODULES_PUBLIC_INTERFACES_H_
 
-#include "triple_banana/modules/public/connector.h"
+#include <string>
+#include "content/public/child/child_thread.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "triple_banana/modules/public/coroutine.h"
+#include "triple_banana/modules/public/module_service.h"
 #include "triple_banana/modules/public/mojom/adblock.mojom.h"
 #include "triple_banana/modules/public/mojom/authentication.mojom.h"
 #include "triple_banana/modules/public/mojom/encrypter.mojom.h"
 #include "triple_banana/modules/public/mojom/hello.mojom.h"
+#include "triple_banana/modules/public/string_view.h"
 
-#define EXPORT_INTERFACE_LIST                   \
-  adblock::mojom::FilterLoader,                 \
-  authentication::mojom::AuthenticationManager, \
-      encrypter::mojom::EncrypterManager, hello::mojom::Hello
+#define AutoBind(interface_name)                                \
+  triple_banana::IsBrowserProcess(__FILE__)                     \
+      ? triple_banana::BindInterfaceOnBrowser<interface_name>() \
+      : triple_banana::BindInterfaceOnRenderer<interface_name>()
+
+#define BIND_HOST_RECEIVER(interface_name)                                \
+  if (auto r = receiver.As<interface_name>()) {                             \
+    ModuleService::Get().GetJavaInterfaces()->GetInterface(std::move(r)); \
+  }
+
+namespace triple_banana {
+
+inline constexpr bool IsBrowserProcess(const string_view& file_name) {
+  return file_name.find("/browser/") != std::string::npos;
+}
+
+template <typename Interface>
+inline mojo::Remote<Interface> BindInterfaceOnRenderer() {
+  mojo::Remote<Interface> remote_interface;
+  content::ChildThread::Get()->BindHostReceiver(
+      remote_interface.BindNewPipeAndPassReceiver());
+  return remote_interface;
+}
+
+template <typename Interface>
+inline mojo::Remote<Interface> BindInterfaceOnBrowser() {
+  mojo::Remote<Interface> remote_interface;
+  ModuleService::Get().GetJavaInterfaces()->GetInterface(
+      remote_interface.BindNewPipeAndPassReceiver());
+  return remote_interface;
+}
+
+inline void OnBindHostReceiverForRenderer(
+    mojo::GenericPendingReceiver receiver) {
+  BIND_HOST_RECEIVER(adblock::mojom::FilterLoader);
+  BIND_HOST_RECEIVER(authentication::mojom::AuthenticationManager);
+  BIND_HOST_RECEIVER(encrypter::mojom::EncrypterManager);
+  BIND_HOST_RECEIVER(hello::mojom::Hello);
+}
+
+}  // namespace triple_banana
 
 #endif  // TRIPLE_BANANA_MODULES_PUBLIC_INTERFACES_H_
