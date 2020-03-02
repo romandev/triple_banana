aa617e67eb1de0d982e038bcfa1135b9f0a68f9b
Jinho Bang <zino@chromium.org>
2020-03-02 14:36:54 +0900

    Disable AImageReader for ARM64 build on P-OS(and later) (#301)
    Currently, 64bit release binary causes a crash when playing video on
    some web site such as YouTube. It is an known issue o Android and hasn't
    been fixed yet[1].
    
    So, this patch simply disables AImageReader feature for ARM64 build on
    P-OS(and later) as a temporary workaround until we have some fix for the
    issue.
    
    [1] https://crbug.com/977583
    
    Refs: #300

--- src/org/triple/banana/CommandLineInitializer.java
+++ src/org/triple/banana/CommandLineInitializer.java
@@ -1,5 +1,12 @@
+// Copyright 2020 The Triple Banana Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package org.triple.banana;
 
+import android.os.Build;
+
+import org.banana.cake.interfaces.BananaBuildConfig;
 import org.banana.cake.interfaces.BananaCommandLine;
 import org.banana.cake.interfaces.BananaCommandLineInitializer;
 import org.triple.banana.settings.ExtensionFeatures;
@@ -8,6 +15,9 @@ import org.triple.banana.settings.ExtensionFeatures.FeatureName;
 class CommandLineInitializer implements BananaCommandLineInitializer {
     @Override
     public void initCommandLine() {
+        if (BananaBuildConfig.IS_ARM64 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            BananaCommandLine.get().appendSwitchWithValue("disable_aimagereader", "1");
+        }
         if (ExtensionFeatures.isEnabled(FeatureName.BOTTOM_TOOLBAR, true)) {
             BananaCommandLine.get().appendSwitchWithValue("enable-features",
                     "ChromeDuet,HomePageButtonForceEnabled,OmniboxSearchEngineLogo");
