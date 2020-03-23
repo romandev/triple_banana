5684a2b364af586339b688fcf091f5893b839222
Jinho Bang <zino@chromium.org>
2020-03-23 20:52:59 +0900

    Implement metadata check for subresource filter (#354)
    Currently, we trigger to download the subresource filter fully once
    every two weeks even if they already have it. It works anyway but it's
    a huge waste of data. So, this patch makes it check the filter's
    metadata(e.g. version, size, and so on) first and then actually request
    to download it if there was a metadata change.
    
    Refs: #348

--- src/org/triple/banana/adblock/FilterLoaderImpl.java
+++ src/org/triple/banana/adblock/FilterLoaderImpl.java
@@ -7,20 +7,23 @@ package org.triple.banana.adblock;
 import android.app.AlarmManager;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.banana.cake.interfaces.BananaApplicationUtils;
 import org.triple.banana.adblock.mojom.FilterLoader;
 import org.triple.banana.download.SimpleDownloader;
+import org.triple.banana.remote_config.RemoteConfig;
 
 import org.chromium.mojo.system.MojoException;
 import org.chromium.services.service_manager.InterfaceFactory;
 
 public class FilterLoaderImpl implements FilterLoader {
     private static final String TAG = "FilterLoaderImpl";
+    private final RemoteConfig mMetaData =
+            new RemoteConfig("https://zino.dev/triple_banana_filter/metadata.json");
 
-    // FIXME(zino): We should modify the following URL but now let's just use it for test.
-    private static final String FILTER_URL = "https://www.bromite.org/filters/filters.dat";
+    private static final Uri FILTER_BASE_URL = Uri.parse("https://zino.dev/triple_banana_filter/");
     private static final String LAST_CHECK_TIME_KEY = "filter_download_last_check_time";
     private static final long UPDATE_INTERVAL = AlarmManager.INTERVAL_DAY * 14;
 
@@ -36,25 +39,30 @@ public class FilterLoaderImpl implements FilterLoader {
     }
 
     @Override
-    public void load(final LoadResponse callback) {
+    public void load(final String currentVersion, final LoadResponse callback) {
         long now = System.currentTimeMillis();
         if (now < getLastCheckTime() + UPDATE_INTERVAL) return;
 
-        // TODO(zino): We might have to consider meta check first because the filter data might be
-        // large.
+        mMetaData.getAsync(info -> {
+            String newVersion = info.optString("version");
+            Log.i(TAG, "load(): current = " + currentVersion + ", new = " + newVersion);
+            if (TextUtils.equals(currentVersion, newVersion)) return;
 
-        // TODO(zino): We might have to consider zip compression.
+            // TODO(zino): We might have to consider zip compression.
 
-        Log.i(TAG, "load(): Try to update filter");
-        SimpleDownloader.get().download(Uri.parse(FILTER_URL), result -> {
-            if (result == null) {
-                callback.call(new String());
-                return;
-            }
+            final long filterSize = info.optLong("size");
+            Uri filterUri = Uri.withAppendedPath(FILTER_BASE_URL, newVersion + ".filter");
+            SimpleDownloader.get().download(filterUri, result -> {
+                if (result == null || result.length() != filterSize) {
+                    Log.i(TAG, "load(): Download failed");
+                    callback.call(new String());
+                    return;
+                }
 
-            Log.i(TAG, "load(): Filter updated");
-            updateLastCheckTime();
-            callback.call(result.getAbsolutePath());
+                Log.i(TAG, "load(): Filter updated");
+                updateLastCheckTime();
+                callback.call(result.getAbsolutePath());
+            });
         });
     }
 
