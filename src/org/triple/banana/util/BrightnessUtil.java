c166f44e1b38eac4c373f085de6ba347daace110
Byoungkwon Ko <codeimpl@gmail.com>
2020-08-06 11:27:24 +0900

    Introduce BrightnessUtil to adjust screen brightness easily (#513)
    It helps the client adjust the screen brightness easily.
    
    Refs: #489
    Co-authored-by: Jaeyong Bae <jdragon.bae@gmail.com>

---
+++ src/org/triple/banana/util/BrightnessUtil.java
@@ -0,0 +1,46 @@
+// Copyright 2020 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.triple.banana.util;
+
+import android.content.Context;
+import android.provider.Settings.System;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class BrightnessUtil {
+    private static final String TAG = "BrightnessUtil";
+
+    public static float getSystemBrightness(Context context) {
+        int brightness = 0;
+
+        try {
+            brightness = System.getInt(
+                    context.getContentResolver(), System.SCREEN_BRIGHTNESS);
+        } catch (Exception e) {
+            Log.e(TAG, "getSystemBrightness(): " + e.toString());
+        }
+
+        return brightness / 255.0f;
+    }
+
+    public static float getWindowBrightness(Window window) {
+        WindowManager.LayoutParams lp = window.getAttributes();
+        return lp.screenBrightness;
+    }
+
+    public static void setWindowBrightness(Window window, float value) {
+        if (value < 0.0f) {
+            value = 0.0f;
+        } else if (value > 1.0f) {
+            value = 1.0f;
+        }
+
+        WindowManager.LayoutParams lp = window.getAttributes();
+        lp.screenBrightness = value;
+        window.setAttributes(lp);
+    }
+}
