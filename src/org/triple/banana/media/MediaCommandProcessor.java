romandev/triple_banana

commit 1ec81937d6c06d8fee629ab2ac36e0f31010aa95

Fix a bug that remote control view's play button doens't work (#536)
Currently, if autoplay feature is disabled, remote control view's play
button is not working. It is caused because the MediaCommandProcessor
can't consume user gesture because the remote control view is overlayed.
So, this patch reimplements the MediaCommandProcessor making it use the
internal MediaSession.

Refs: #535

src/org/triple/banana/media/MediaCommandProcessor.java
@@ -5,6 +5,9 @@
 
 package org.triple.banana.media;
 
+import androidx.annotation.NonNull;
+
+import org.banana.cake.interfaces.BananaMediaCommandProcessor;
 import org.banana.cake.interfaces.BananaTab;
 
 public enum MediaCommandProcessor {
@@ -20,20 +23,28 @@ public enum MediaCommandProcessor {
         tab.evaluateJavaScript(commandBuilder.toString());
     }
 
+    private @NonNull BananaMediaCommandProcessor getCommandProcessor() {
+        BananaTab tab = org.banana.cake.interfaces.BananaTabManager.get().getActivityTab();
+        if (tab != null) return tab.getMediaCommandProcessor();
+
+        // Returns empty media command processor (no operations)
+        return BananaTab.get().getMediaCommandProcessor();
+    }
+
     public void play() {
-        runMediaCommand("play()");
+        getCommandProcessor().play();
     }
 
     public void pause() {
-        runMediaCommand("pause()");
+        getCommandProcessor().pause();
     }
 
-    public void setPosition(float position) {
-        runMediaCommand("currentTime = " + Float.toString(position));
+    public void setPosition(long ms) {
+        getCommandProcessor().setPosition(ms);
     }
 
-    public void setRelativePosition(float position) {
-        runMediaCommand("currentTime += " + Float.toString(position));
+    public void setRelativePosition(long ms) {
+        getCommandProcessor().setRelativePosition(ms);
     }
 
     public void setVolume(float value) {

BUILD.gn
@@ -73,6 +73,7 @@ android_library("triple_banana_java") {
     "//triple_banana/banana_cake/java/org/banana/cake/CakeDarkModeUtils.java",
     "//triple_banana/banana_cake/java/org/banana/cake/CakeFeatureFlags.java",
     "//triple_banana/banana_cake/java/org/banana/cake/CakeInterfaceProvider.java",
+    "//triple_banana/banana_cake/java/org/banana/cake/CakeMediaCommandProcessor.java",
     "//triple_banana/banana_cake/java/org/banana/cake/CakeTab.java",
     "//triple_banana/banana_cake/java/org/banana/cake/CakeTabManager.java",
     "//triple_banana/banana_cake/java/org/banana/cake/CakeToolbarManager.java",

banana_cake/interfaces/java/org/banana/cake/interfaces/BananaMediaCommandProcessor.java
@@ -1,0 +1,13 @@
+// Copyright 2020 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.banana.cake.interfaces;
+
+public interface BananaMediaCommandProcessor {
+    void play();
+    void pause();
+    void setPosition(long ms);
+    void setRelativePosition(long ms);
+}

banana_cake/interfaces/java/org/banana/cake/interfaces/BananaTab.java
@@ -7,6 +7,8 @@ package org.banana.cake.interfaces;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
 import org.chromium.chrome.browser.tab.Tab;
 
 public interface BananaTab {
@@ -20,4 +22,6 @@ public interface BananaTab {
     void evaluateJavaScript(String script);
     Context getContext();
     void exitFullscreen();
+    @NonNull
+    BananaMediaCommandProcessor getMediaCommandProcessor();
 }

banana_cake/java/org/banana/cake/CakeMediaCommandProcessor.java
@@ -1,0 +1,44 @@
+// Copyright 2020 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.banana.cake;
+
+import androidx.annotation.Nullable;
+
+import org.banana.cake.interfaces.BananaMediaCommandProcessor;
+
+import org.chromium.content_public.browser.MediaSession;
+
+class CakeMediaCommandProcessor implements BananaMediaCommandProcessor {
+    private final @Nullable MediaSession mSession;
+
+    CakeMediaCommandProcessor(MediaSession session) {
+        mSession = session;
+    }
+
+    @Override
+    public void play() {
+        if (mSession == null) return;
+        mSession.resume();
+    }
+
+    @Override
+    public void pause() {
+        if (mSession == null) return;
+        mSession.suspend();
+    }
+
+    @Override
+    public void setPosition(long ms) {
+        if (mSession == null) return;
+        mSession.seek(ms);
+    }
+
+    @Override
+    public void setRelativePosition(long ms) {
+        if (mSession == null) return;
+        mSession.seekTo(ms);
+    }
+}

banana_cake/java/org/banana/cake/CakeTab.java
@@ -7,9 +7,13 @@ package org.banana.cake;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
+
+import org.banana.cake.interfaces.BananaMediaCommandProcessor;
 import org.banana.cake.interfaces.BananaTab;
 
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.content_public.browser.MediaSession;
 import org.chromium.url.GURL;
 
 class CakeTab implements BananaTab {
@@ -53,5 +57,13 @@ class CakeTab implements BananaTab {
     public void exitFullscreen() {
         if (mTab == null || mTab.getWebContents() == null) return;
         mTab.getWebContents().exitFullscreen();
+    }
+
+    @Override
+    public @NonNull BananaMediaCommandProcessor getMediaCommandProcessor() {
+        if (mTab == null || mTab.getWebContents() == null) {
+            return new CakeMediaCommandProcessor(null);
+        }
+        return new CakeMediaCommandProcessor(MediaSession.fromWebContents(mTab.getWebContents()));
     }
 }

src/org/triple/banana/media/MediaController.java
@@ -27,12 +27,12 @@ public enum MediaController {
         mCommandProcessor.pause();
     }
 
-    public void setPosition(float position) {
-        mCommandProcessor.setPosition(position);
+    public void setPosition(long ms) {
+        mCommandProcessor.setPosition(ms);
     }
 
-    public void setRelativePosition(float position) {
-        mCommandProcessor.setRelativePosition(position);
+    public void setRelativePosition(long ms) {
+        mCommandProcessor.setRelativePosition(ms);
     }
 
     public void setVolume(float value) {

src/org/triple/banana/remote_control/RemoteControlService.java
@@ -65,9 +65,9 @@ public enum RemoteControlService implements RemoteControlView.Delegate {
         } else if (id == R.id.pause_button) {
             mMediaController.pause();
         } else if (id == R.id.backward_button) {
-            mMediaController.setRelativePosition(-10.0f);
+            mMediaController.setRelativePosition(-10000);
         } else if (id == R.id.forward_button) {
-            mMediaController.setRelativePosition(10.0f);
+            mMediaController.setRelativePosition(10000);
         } else if (id == R.id.brightness_up_button) {
             mViewModel.getEditor().setBrightness(1.0f);
             mViewModel.commit();

commit d5c5486b6f9eacba41e2b9359158b32da19a2a8b

Use runMediaCommand instead of BananaMediaCommandProcessor (#592)
`BananaMediaCommandProcessor` is not working if media session was gone.
This is a regression after #536. So, this patch partially reverts the
patch and leave a `FIXME` comment.

Refs: #535, #554

src/org/triple/banana/media/MediaCommandProcessor.java
@@ -32,19 +32,21 @@ public enum MediaCommandProcessor {
     }
 
     public void play() {
-        getCommandProcessor().play();
+        // FIXME(#535): If the autoplay feature is disabled, then this operation is not working
+        // because the play() in JavaScript is blocked because of user gesture required.
+        runMediaCommand("play()");
     }
 
     public void pause() {
-        getCommandProcessor().pause();
+        runMediaCommand("pause()");
     }
 
-    public void setPosition(long ms) {
-        getCommandProcessor().setPosition(ms);
+    public void setPosition(float value) {
+        runMediaCommand("currentTime = " + Float.toString(value));
     }
 
-    public void setRelativePosition(long ms) {
-        getCommandProcessor().setRelativePosition(ms);
+    public void setRelativePosition(float value) {
+        runMediaCommand("currentTime += " + Float.toString(value));
     }
 
     public void setVolume(float value) {