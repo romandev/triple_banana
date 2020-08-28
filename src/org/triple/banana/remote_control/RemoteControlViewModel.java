romandev/triple_banana

commit b54790fdccda998d8cd3cbb10ab29b0ec7378d9e

Make the play/pause buttons toggled (#615)
This patch means making the play/pause buttons toggled by using state.
The state in view model will be in 3 such as PLAY, PAUSE, WAITING. When
waiting state, the progress bar will be show and hide the middle
control.

Refs: #584
Co-authored-by: Byoungkwon Ko <codeimpl@gmail.com>
Co-authored-by: Jinho Bang <zino@chromium.org>

src/org/triple/banana/remote_control/RemoteControlViewModel.java
@@ -7,6 +7,7 @@ package org.triple.banana.remote_control;
 
 import android.util.Log;
 
+import org.triple.banana.media.MediaPlayState;
 import org.triple.banana.util.AudioUtil;
 import org.triple.banana.util.BrightnessUtil;
 
@@ -14,6 +15,7 @@ import java.util.HashSet;
 
 class RemoteControlViewModel {
     private static final String TAG = "RemoteControlViewModel";
+
     static class ReadonlyData {
         protected float mBrightness;
         protected float mVolume;
@@ -23,6 +25,7 @@ class RemoteControlViewModel {
         protected boolean mIsLocked;
         protected boolean mVolumeControlVisibility;
         protected boolean mBrightnessControlVisibility;
+        protected MediaPlayState mPlayState;
 
         float getBrightness() {
             return mBrightness;
@@ -54,6 +57,10 @@ class RemoteControlViewModel {
 
         boolean getBrightnessControlVisibility() {
             return mBrightnessControlVisibility;
+        }
+
+        MediaPlayState getPlayState() {
+            return mPlayState;
         }
     }
 
@@ -112,6 +119,10 @@ class RemoteControlViewModel {
 
         void setBrightnessControlVisibility(boolean visibility) {
             mBrightnessControlVisibility = visibility;
+        }
+
+        void setPlayState(MediaPlayState state) {
+            mPlayState = state;
         }
 
         Data cloneData() {

BUILD.gn
@@ -234,10 +234,10 @@ android_resources("triple_banana_resources") {
     "//triple_banana/res/drawable/ic_lock_opened.xml",
     "//triple_banana/res/drawable/ic_mute.xml",
     "//triple_banana/res/drawable/ic_new.xml",
-    "//triple_banana/res/drawable/ic_pause_48dp.xml",
+    "//triple_banana/res/drawable/ic_pause.xml",
     "//triple_banana/res/drawable/ic_phone_black_24dp.xml",
     "//triple_banana/res/drawable/ic_pip.xml",
-    "//triple_banana/res/drawable/ic_play_96dp.xml",
+    "//triple_banana/res/drawable/ic_play.xml",
     "//triple_banana/res/drawable/ic_power_off_black.xml",
     "//triple_banana/res/drawable/ic_refresh_black_24dp.xml",
     "//triple_banana/res/drawable/ic_rewind_48dp.xml",

res/layout/remote_control_view.xml
@@ -16,6 +16,14 @@
         android:layout_alignParentLeft="true"
         android:src="@drawable/ic_lock" />
 
+    <ProgressBar
+        android:id="@+id/waiting_progress_bar"
+        style="?android:attr/progressBarStyle"
+        android:layout_width="96dp"
+        android:layout_height="96dp"
+        android:layout_centerInParent="true"
+        android:indeterminateTint="@color/progress_color" />
+
     <FrameLayout
         android:id="@+id/brightness_layout"
         android:layout_width="@dimen/vertical_seek_bar_length"
@@ -131,14 +139,6 @@
                 android:layout_marginRight="@dimen/margin_option_button"
                 android:layout_toLeftOf="@id/brightness_down_button"
                 android:src="@drawable/ic_brightness_up" />
-
-            <ImageButton
-                android:id="@+id/pause_button"
-                style="@style/BananaMediaButtonTheme"
-                android:layout_marginRight="@dimen/margin_option_button"
-                android:layout_toLeftOf="@id/brightness_up_button"
-                android:src="@drawable/ic_pause_48dp" />
-
         </RelativeLayout>
 
         <RelativeLayout
@@ -152,7 +152,7 @@
                 style="@style/BananaMediaButtonTheme"
                 android:layout_width="wrap_content"
                 android:layout_height="wrap_content"
-                android:src="@drawable/ic_play_96dp" />
+                android:src="@drawable/ic_play" />
 
             <ImageButton
                 android:id="@+id/backward_button"

src/org/triple/banana/remote_control/RemoteControlService.java
@@ -16,6 +16,7 @@ import org.banana.cake.interfaces.BananaTab;
 import org.triple.banana.R;
 import org.triple.banana.media.MediaController;
 import org.triple.banana.media.MediaEventListener;
+import org.triple.banana.media.MediaPlayState;
 import org.triple.banana.util.AudioUtil;
 
 public enum RemoteControlService implements RemoteControlView.Delegate {
@@ -30,6 +31,12 @@ public enum RemoteControlService implements RemoteControlView.Delegate {
     public void start() {
         mViewModel.addListener(mView);
         mMediaController.addEventListener(new MediaEventListener() {
+            @Override
+            public void onPlayStateChanged(MediaPlayState state) {
+                mViewModel.getEditor().setPlayState(state);
+                mViewModel.commit();
+            }
+
             @Override
             public void onEnteredVideoFullscreen() {
                 BananaTab tab = org.banana.cake.interfaces.BananaTabManager.get().getActivityTab();
@@ -65,9 +72,11 @@ public enum RemoteControlService implements RemoteControlView.Delegate {
     @Override
     public void onRemoteControlButtonClicked(int id) {
         if (id == R.id.play_button) {
-            mMediaController.play();
-        } else if (id == R.id.pause_button) {
-            mMediaController.pause();
+            if (mViewModel.getEditor().getPlayState() == MediaPlayState.PAUSED) {
+                mMediaController.play();
+            } else {
+                mMediaController.pause();
+            }
         } else if (id == R.id.backward_button) {
             onBackward();
         } else if (id == R.id.forward_button) {

src/org/triple/banana/remote_control/RemoteControlViewImpl.java
@@ -12,6 +12,7 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
@@ -19,6 +20,7 @@ import androidx.annotation.Nullable;
 
 import org.banana.cake.interfaces.BananaTab;
 import org.triple.banana.R;
+import org.triple.banana.media.MediaPlayState;
 import org.triple.banana.util.BrightnessUtil;
 
 import java.lang.ref.WeakReference;
@@ -64,6 +66,7 @@ class RemoteControlViewImpl implements RemoteControlView, RemoteControlViewModel
         updateVolumeUI(data.getVolumeControlVisibility(), data.getVolume());
         showControls(data.getControlsVisibility(), data.getIsLocked(), data.getIsVolumeMuted());
         setPosition(data.getPosition());
+        setPlayState(data.getPlayState());
     }
 
     private void setPosition(float position) {
@@ -124,6 +127,30 @@ class RemoteControlViewImpl implements RemoteControlView, RemoteControlViewModel
         muteButton.setImageResource(isMuted ? R.drawable.ic_mute : R.drawable.ic_volume_up);
     }
 
+    private void setPlayState(MediaPlayState state) {
+        final ViewGroup middleControl = mMainView.findViewById(R.id.middle_control);
+        final ImageButton playButton = mMainView.findViewById(R.id.play_button);
+        final ProgressBar waitingProgressBar = mMainView.findViewById(R.id.waiting_progress_bar);
+
+        if (middleControl == null || playButton == null || waitingProgressBar == null) return;
+        switch (state) {
+            case PLAYING:
+                middleControl.setVisibility(View.VISIBLE);
+                waitingProgressBar.setVisibility(View.INVISIBLE);
+                playButton.setImageResource(R.drawable.ic_pause);
+                break;
+            case PAUSED:
+                middleControl.setVisibility(View.VISIBLE);
+                waitingProgressBar.setVisibility(View.INVISIBLE);
+                playButton.setImageResource(R.drawable.ic_play);
+                break;
+            case WAITING:
+                middleControl.setVisibility(View.INVISIBLE);
+                waitingProgressBar.setVisibility(View.VISIBLE);
+                break;
+        }
+    }
+
     private void initializeDialogIfNeeded(@NonNull Context context) {
         if (mDialog != null) return;
         mDialog = new Dialog(context, R.style.Theme_Banana_Fullscreen_Transparent_Dialog);
@@ -159,7 +186,6 @@ class RemoteControlViewImpl implements RemoteControlView, RemoteControlViewModel
         });
 
         mDialog.findViewById(R.id.play_button).setOnClickListener(mClickListener);
-        mDialog.findViewById(R.id.pause_button).setOnClickListener(mClickListener);
         mDialog.findViewById(R.id.backward_button).setOnClickListener(mClickListener);
         mDialog.findViewById(R.id.forward_button).setOnClickListener(mClickListener);
         mDialog.findViewById(R.id.brightness_up_button).setOnClickListener(mClickListener);

commit 006eba54aef38f3ea8005a6efc66afe66149f901

Fix a crash when entering remote control service (#617)
This is a regression since #615. We should initialize the variable.

Refs: none

src/org/triple/banana/remote_control/RemoteControlViewModel.java
@@ -25,7 +25,7 @@ class RemoteControlViewModel {
         protected boolean mIsLocked;
         protected boolean mVolumeControlVisibility;
         protected boolean mBrightnessControlVisibility;
-        protected MediaPlayState mPlayState;
+        protected MediaPlayState mPlayState = MediaPlayState.PAUSED;
 
         float getBrightness() {
             return mBrightness;