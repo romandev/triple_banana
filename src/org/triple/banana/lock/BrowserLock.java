romandev/triple_banana

commit e8fa4ce99ca0a217a32e907bdf23e5788188fa10

Introduce shutdown() API in BananaApplicationUtils (#720)
Use shutdown() instead of moveTaskToBack(true);

Refs: #712

src/org/triple/banana/lock/BrowserLock.java
@@ -47,7 +47,7 @@ public class BrowserLock {
                 if (result) {
                     recoredLastAuthenticationTime();
                 } else {
-                    lastActivity.moveTaskToBack(true);
+                    BananaApplicationUtils.get().shutdown();
                 }
             });
         }

banana_cake/interfaces/java/org/banana/cake/interfaces/BananaApplicationUtils.java
@@ -27,6 +27,7 @@ public interface BananaApplicationUtils {
     void unregisterActivityStateListener(BananaActivityStateListener listener);
     boolean hasVisibleActivities();
     void restart();
+    void shutdown();
     AlertDialog.Builder getDialogBuilder(Context context);
     void showInfoPage(String url);
 }

banana_cake/java/org/banana/cake/CakeApplicationUtils.java
@@ -52,6 +52,11 @@ class CakeApplicationUtils implements BananaApplicationUtils {
     }
 
     @Override
+    public void shutdown() {
+        ApplicationLifetime.terminate(false);
+    }
+
+    @Override
     public AlertDialog.Builder getDialogBuilder(Context context) {
         return new UiUtils.CompatibleAlertDialogBuilder(
                 context, R.style.Theme_Chromium_AlertDialog);

commit 3720089de00651484bed1c40a6d742290633949b

Fix a build error (#721)
This is caused by #720.

Refs: #712

src/org/triple/banana/lock/BrowserLock.java
@@ -5,6 +5,7 @@
 
 package org.triple.banana.lock;
 
+import org.banana.cake.interfaces.BananaApplicationUtils;
 import org.triple.banana.authentication.Authenticator;
 import org.triple.banana.authentication.SecurityLevelChecker;
 import org.triple.banana.base.ApplicationStatusTracker;