romandev/triple_banana

commit 60ce59a2db358d94ab3e3b8f97cedfb9f7ca0cee

Add Tab Switcher button on the bottom toolbar (#773)
Tab Switcher button exists on the top toolbar. But it is not easy to
handle it. So this patch adds the button to the bottom toolbar.

Refs: #772
Co-authored-by: Jaeyong Bae <jdragon.bae@gmail.com>

src/org/triple/banana/toolbar/BottomToolbarController.java
@@ -64,13 +64,6 @@ public class BottomToolbarController implements BananaBottomToolbarController,
             }
         });
 
-        // Create all toolbar buttons when toolbar controller initialized
-        for (ButtonId buttonId : ButtonId.values()) {
-            mToolbarButtons.put(buttonId, createToolbarButton(buttonId));
-        }
-
-        ToolbarStateModel.getInstance().addObserver(this);
-        ToolbarStateModel.getInstance().notifyObservers(); // only first time
 
         return this;
     }
@@ -88,8 +81,17 @@ public class BottomToolbarController implements BananaBottomToolbarController,
     }
 
     private ToolbarButton createToolbarButton(ButtonId buttonId) {
-        ToolbarButton toolbarButton =
-                new ToolbarButton(BananaApplicationUtils.get().getApplicationContext());
+        ToolbarButton toolbarButton;
+        if (buttonId == ButtonId.TAB_SWITCHER) {
+            toolbarButton = new TabSwitcherToolbarButton(
+                    BananaApplicationUtils.get().getApplicationContext());
+            toolbarButton.setOnClickListener(mTabSwitcherListener);
+        } else {
+            toolbarButton = new ToolbarButton(BananaApplicationUtils.get().getApplicationContext());
+            if (ButtonId.getOnClickListeners(buttonId) != null) {
+                toolbarButton.setOnClickListener(ButtonId.getOnClickListeners(buttonId));
+            }
+        }
         int width = (int) TypedValue.applyDimension((TypedValue.COMPLEX_UNIT_DIP), 56,
                 BananaApplicationUtils.get()
                         .getApplicationContext()
@@ -99,9 +101,6 @@ public class BottomToolbarController implements BananaBottomToolbarController,
                 new ViewGroup.LayoutParams(width, ViewGroup.LayoutParams.MATCH_PARENT));
         toolbarButton.setButtonId(buttonId);
         toolbarButton.setImageResource(ButtonId.getImageResource(buttonId));
-        if (ButtonId.getOnClickListeners(buttonId) != null) {
-            toolbarButton.setOnClickListener(ButtonId.getOnClickListeners(buttonId));
-        }
         if (ButtonId.getOnClickListeners(buttonId) != null) {
             toolbarButton.setOnLongClickListener(ButtonId.getOnLongClickListeners(buttonId));
         }
@@ -119,9 +118,24 @@ public class BottomToolbarController implements BananaBottomToolbarController,
     }
 
     private void buttonInitializeWithNative() {
+        // Create all toolbar buttons when toolbar controller initialized
+        for (ButtonId buttonId : ButtonId.values()) {
+            mToolbarButtons.put(buttonId, createToolbarButton(buttonId));
+        }
+
+        ToolbarStateModel.getInstance().addObserver(this);
+        ToolbarStateModel.getInstance().notifyObservers(); // only first time
+
         for (ToolbarButton button : mToolbarButtons.values()) {
             button.setThemeColorProvider(mThemeColorProvider);
         }
+
+        mTabCountProvider.addObserverAndTrigger((tabCount, isIncognito) -> {
+            if (mToolbarButtons.containsKey(ButtonId.TAB_SWITCHER)) {
+                ((TabSwitcherToolbarButton) mToolbarButtons.get(ButtonId.TAB_SWITCHER))
+                        .updateForTabCount(tabCount, isIncognito);
+            }
+        });
 
         mThemeColorProvider.addThemeColorObserver(this);
         mIncognitoStateProvider.addIncognitoStateObserverAndTrigger(this);

BUILD.gn
@@ -199,6 +199,7 @@ android_library("triple_banana_java") {
     "//triple_banana/src/org/triple/banana/toolbar/IToolbarStatePersistentStore.java",
     "//triple_banana/src/org/triple/banana/toolbar/OnItemMoveListener.java",
     "//triple_banana/src/org/triple/banana/toolbar/SelectedToolbarButtonAdapter.java",
+    "//triple_banana/src/org/triple/banana/toolbar/TabSwitcherToolbarButton.java",
     "//triple_banana/src/org/triple/banana/toolbar/ToolbarButton.java",
     "//triple_banana/src/org/triple/banana/toolbar/ToolbarButtonItem.java",
     "//triple_banana/src/org/triple/banana/toolbar/ToolbarEditActivity.java",

src/org/triple/banana/toolbar/ButtonId.java
@@ -36,7 +36,8 @@ public enum ButtonId {
     TERMINATE,
     AT_ME_GAME,
     CLEAR_BROWSING_DATA,
-    CLOSE_TAB;
+    CLOSE_TAB,
+    TAB_SWITCHER;
 
     static EnumMap<ButtonId, View.OnClickListener> sOnClickListeners =
             new EnumMap<>(ButtonId.class);
@@ -147,6 +148,9 @@ public enum ButtonId {
                 break;
             case CLOSE_TAB:
                 imageResource = R.drawable.ic_close_current_tab;
+                break;
+            case TAB_SWITCHER:
+                imageResource = R.drawable.btn_tabswitcher_modern;
                 break;
         }
         return imageResource;

src/org/triple/banana/toolbar/TabSwitcherToolbarButton.java
@@ -1,0 +1,51 @@
+// Copyright 2020 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.triple.banana.toolbar;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.util.AttributeSet;
+
+import androidx.annotation.DrawableRes;
+
+import org.chromium.chrome.browser.toolbar.TabSwitcherDrawable;
+
+class TabSwitcherToolbarButton extends ToolbarButton {
+    private TabSwitcherDrawable mTabSwitcherButtonDrawable;
+
+    public TabSwitcherToolbarButton(Context context) {
+        this(context, null);
+    }
+
+    public TabSwitcherToolbarButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public TabSwitcherToolbarButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setImageResource(@DrawableRes int resId) {
+        mTabSwitcherButtonDrawable =
+                TabSwitcherDrawable.createTabSwitcherDrawable(getContext(), false);
+        getImageButton().setImageDrawable(mTabSwitcherButtonDrawable);
+    }
+
+    public void updateForTabCount(int tabCount, boolean isIncognito) {
+        if (mTabSwitcherButtonDrawable != null) {
+            mTabSwitcherButtonDrawable.updateForTabCount(tabCount, isIncognito);
+        }
+    }
+
+    @Override
+    public void onTintChanged(ColorStateList tint, boolean useLight) {
+        super.onTintChanged(tint, useLight);
+        if (mTabSwitcherButtonDrawable != null) {
+            mTabSwitcherButtonDrawable.setTint(tint);
+        }
+    }
+}

commit ab91bbc9c779147bf4be9fbda14e47ef358d96fa

Fix a bug the upstream toolbar appears for a while (#783)
This patch means fixing a bug the upstream toolbar appears for a while. It's
regression after #773. At this patch, we moved creating toolbar buttons in
`buttonInitializeWithNative()` to set listener `mTabSwitcherListener`. This
changes the timing for `viewGroup.removeAllViews()`, it turns out the upstream
toolbar appears for a while. So, we just do set `mTabSwitcherListener` on
`buttonInitializeWithNative()`.

Refs: #782

src/org/triple/banana/toolbar/BottomToolbarController.java
@@ -64,6 +64,13 @@ public class BottomToolbarController implements BananaBottomToolbarController,
             }
         });
 
+        // Create all toolbar buttons when toolbar controller initialized
+        for (ButtonId buttonId : ButtonId.values()) {
+            mToolbarButtons.put(buttonId, createToolbarButton(buttonId));
+        }
+
+        ToolbarStateModel.getInstance().addObserver(this);
+        ToolbarStateModel.getInstance().notifyObservers(); // only first time
 
         return this;
     }
@@ -85,7 +92,6 @@ public class BottomToolbarController implements BananaBottomToolbarController,
         if (buttonId == ButtonId.TAB_SWITCHER) {
             toolbarButton = new TabSwitcherToolbarButton(
                     BananaApplicationUtils.get().getApplicationContext());
-            toolbarButton.setOnClickListener(mTabSwitcherListener);
         } else {
             toolbarButton = new ToolbarButton(BananaApplicationUtils.get().getApplicationContext());
             if (ButtonId.getOnClickListeners(buttonId) != null) {
@@ -118,15 +124,10 @@ public class BottomToolbarController implements BananaBottomToolbarController,
     }
 
     private void buttonInitializeWithNative() {
-        // Create all toolbar buttons when toolbar controller initialized
-        for (ButtonId buttonId : ButtonId.values()) {
-            mToolbarButtons.put(buttonId, createToolbarButton(buttonId));
-        }
-
-        ToolbarStateModel.getInstance().addObserver(this);
-        ToolbarStateModel.getInstance().notifyObservers(); // only first time
-
         for (ToolbarButton button : mToolbarButtons.values()) {
+            if (button.getButtonId() == ButtonId.TAB_SWITCHER) {
+                button.setOnClickListener(mTabSwitcherListener);
+            }
             button.setThemeColorProvider(mThemeColorProvider);
         }
 