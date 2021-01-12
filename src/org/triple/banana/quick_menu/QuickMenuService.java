602b0cf1afc45e730022768592876b0ecd4529cc
Jinho Bang <zino@chromium.org>
2021-01-12 15:14:12 +0900

    Implement QuickMenuService

---
+++ src/org/triple/banana/quick_menu/QuickMenuService.java
@@ -0,0 +1,18 @@
+// Copyright 2021 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.triple.banana.quick_menu;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public interface QuickMenuService {
+    static @NonNull QuickMenuService create() {
+        return new QuickMenuServiceImpl();
+    }
+
+    void show(@NonNull Context context);
+}
---
+++ src/org/triple/banana/quick_menu/QuickMenuServiceImpl.java
@@ -0,0 +1,17 @@
+// Copyright 2021 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.triple.banana.quick_menu;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+class QuickMenuServiceImpl implements QuickMenuService {
+    @Override
+    public void show(@NonNull Context context) {
+        // NOTIMPLEMENTED
+    }
+}
