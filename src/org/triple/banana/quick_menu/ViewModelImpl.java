6d9bdb1bf4916554da8a8680d5419c1a7ac6f24f
Jinho Bang <zino@chromium.org>
2021-01-12 22:18:54 +0900

    Implement ViewModel

---
+++ src/org/triple/banana/quick_menu/ViewModelImpl.java
@@ -0,0 +1,18 @@
+// Copyright 2021 The Triple Banana Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package org.triple.banana.quick_menu;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+class ViewModelImpl extends ViewModel implements ViewModelData {
+    @Override
+    public @NonNull List<ButtonInfo> getButtons() {
+        // NOTIMPLEMENTED
+        return null;
+    }
+}
