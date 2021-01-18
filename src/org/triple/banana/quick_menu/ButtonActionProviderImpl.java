c6e0368d66067c98bf3f74dd2b1eadc320b6682b
Jinho Bang <zino@chromium.org>
2021-01-18 18:20:08 +0900

    Adjust the order of the button list in quick menu (#1032)
    Refs: #974

--- src/org/triple/banana/quick_menu/ButtonActionProviderImpl.java
+++ src/org/triple/banana/quick_menu/ButtonActionProviderImpl.java
@@ -25,25 +25,20 @@ class ButtonActionProviderImpl implements ButtonActionProvider {
     ButtonActionProviderImpl() {
         mActionMap = new HashMap<Integer, Runnable>() {
             {
+                // FIXME(#1031): We should add actions for SecureDNS and AdBlock.
+
                 put(R.id.add_secret_tab, BananaToolbarManager.get()::addSecretTab);
                 put(R.id.add_to_home, BananaToolbarManager.get()::addToHomeScreen);
                 put(R.id.archive, BananaToolbarManager.get()::goArchive);
-                put(R.id.at_me_game,
-                        ()
-                                -> BananaApplicationUtils.get().showInfoPage(
-                                        "https://www.atmegame.com/?utm_source=Banana&utm_medium=Banana"));
-                put(R.id.back, BananaToolbarManager.get()::back);
                 put(R.id.clear_data, BananaToolbarManager.get()::openClearBrowsingDataPreference);
                 put(R.id.close_tab, BananaToolbarManager.get()::closeCurrentTab);
                 put(R.id.dark_mode, DarkModeController.get()::toggle);
                 put(R.id.desktop_view, BananaToolbarManager.get()::changeDesktopMode);
                 put(R.id.download, BananaToolbarManager.get()::download);
                 put(R.id.find_in_page, BananaToolbarManager.get()::findInPage);
-                put(R.id.forward, BananaToolbarManager.get()::forward);
                 put(R.id.new_tab, BananaToolbarManager.get()::addNewTab);
                 put(R.id.print, BananaToolbarManager.get()::print);
                 put(R.id.qr_code, BananaToolbarManager.get()::openQRCodeDialog);
-                put(R.id.reload, BananaToolbarManager.get()::reload);
                 put(R.id.search, BananaToolbarManager.get()::search);
                 put(R.id.share, BananaToolbarManager.get()::share);
                 put(R.id.terminate, () -> {
--- src/org/triple/banana/quick_menu/ButtonInfoStorageImpl.java
+++ src/org/triple/banana/quick_menu/ButtonInfoStorageImpl.java
@@ -19,37 +19,30 @@ class ButtonInfoStorageImpl implements ButtonInfoStorage, Model.Data {
     ButtonInfoStorageImpl() {
         mButtons = new ArrayList<ButtonInfo>() {
             {
-                add(new ButtonInfo(R.id.add_secret_tab,
-                        R.drawable.ic_add_secret_tab_black_24dp, R.string.add_secret_tab));
-                add(new ButtonInfo(R.id.add_to_home,
-                        R.drawable.ic_add_to_home_black_24dp, R.string.add_to_home));
+                add(new ButtonInfo(R.id.adblock,
+                        R.drawable.adblock, R.string.adblock));
+                add(new ButtonInfo(R.id.secure_dns,
+                        R.drawable.vpn, R.string.secure_dns));
                 add(new ButtonInfo(
                         R.id.archive, R.drawable.ic_archive_black_24dp, R.string.archive));
                 add(new ButtonInfo(
-                        R.id.at_me_game, R.drawable.atmegame_logo, R.string.at_me_game));
-                add(new ButtonInfo(R.id.back, R.drawable.ic_arrow_backward, R.string.back));
-                add(new ButtonInfo(
-                        R.id.clear_data, R.drawable.ic_clear_browsing_data, R.string.clear_data));
-                add(new ButtonInfo(
-                        R.id.close_tab, R.drawable.ic_close_current_tab, R.string.close_tab));
-                add(new ButtonInfo(
-                        R.id.dark_mode, R.drawable.ic_dark_black_24dp, R.string.dark_mode));
-                add(new ButtonInfo(R.id.desktop_view, R.drawable.ic_desktop_black_24dp,
-                        R.string.desktop_view));
+                        R.id.visit_history, R.drawable.ic_visit_history_black_24dp, R.string.visit_history));
+                add(new ButtonInfo(R.id.add_to_home,
+                        R.drawable.ic_add_to_home_black_24dp, R.string.add_to_home));
                 add(new ButtonInfo(
                         R.id.download, R.drawable.ic_download, R.string.download));
+                add(new ButtonInfo(
+                        R.id.dark_mode, R.drawable.ic_dark_black_24dp, R.string.dark_mode));
                 add(new ButtonInfo(R.id.find_in_page,
                         R.drawable.ic_find_in_page_black_24dp, R.string.find_in_page));
-                add(new ButtonInfo(
-                        R.id.forward, R.drawable.ic_arrow_forward, R.string.forward));
-                add(new ButtonInfo(
-                        R.id.new_tab, R.drawable.ic_add_black_24dp, R.string.new_tab));
                 add(new ButtonInfo(
                         R.id.print, R.drawable.ic_local_printshop_black_24dp, R.string.print));
                 add(new ButtonInfo(
                         R.id.qr_code, R.drawable.qr_code, R.string.qr_code));
                 add(new ButtonInfo(
-                        R.id.reload, R.drawable.ic_refresh_black_24dp, R.string.reload));
+                        R.id.share, R.drawable.ic_share_black_24dp, R.string.share));
+                add(new ButtonInfo(R.id.desktop_view, R.drawable.ic_desktop_black_24dp,
+                        R.string.desktop_view));
             }
         };
     }
