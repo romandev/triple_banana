3db0e7568fe417f6b6a7336b3c3655fbef5b9e30
Jinho Bang <zino@chromium.org>
2021-01-20 14:58:38 +0900

    Change the order of the icons in QuickMenu (#1057)
    Refs: #1047

--- src/org/triple/banana/quick_menu/ButtonInfoStorageImpl.java
+++ src/org/triple/banana/quick_menu/ButtonInfoStorageImpl.java
@@ -24,23 +24,20 @@ class ButtonInfoStorageImpl implements ButtonInfoStorage, Model.Data {
                 add(new ButtonInfo(R.id.secure_dns,
                         R.drawable.secure_dns, R.string.secure_dns));
                 add(new ButtonInfo(
-                        R.id.archive, R.drawable.ic_archive_black_24dp, R.string.archive));
-                add(new ButtonInfo(
-                        R.id.visit_history, R.drawable.ic_visit_history_black_24dp, R.string.visit_history));
-                add(new ButtonInfo(R.id.add_to_home,
-                        R.drawable.ic_add_to_home_black_24dp, R.string.add_to_home));
-                add(new ButtonInfo(
-                        R.id.download, R.drawable.ic_download, R.string.download));
-                add(new ButtonInfo(
                         R.id.dark_mode, R.drawable.ic_dark_black_24dp, R.string.dark_mode));
-                add(new ButtonInfo(R.id.find_in_page,
-                        R.drawable.ic_find_in_page_black_24dp, R.string.find_in_page));
                 add(new ButtonInfo(
                         R.id.media_feature, R.drawable.media_feature, R.string.media_features));
                 add(new ButtonInfo(
-                        R.id.qr_code, R.drawable.qr_code, R.string.qr_code));
+                        R.id.visit_history, R.drawable.ic_visit_history_black_24dp, R.string.visit_history));
                 add(new ButtonInfo(
-                        R.id.share, R.drawable.ic_share_black_24dp, R.string.share));
+                        R.id.archive, R.drawable.ic_archive_black_24dp, R.string.archive));
+                add(new ButtonInfo(R.id.download, R.drawable.ic_download, R.string.download));
+                add(new ButtonInfo(R.id.add_to_home,
+                        R.drawable.ic_add_to_home_black_24dp, R.string.add_to_home));
+                add(new ButtonInfo(R.id.share, R.drawable.ic_share_black_24dp, R.string.share));
+                add(new ButtonInfo(R.id.qr_code, R.drawable.qr_code, R.string.qr_code));
+                add(new ButtonInfo(R.id.find_in_page,
+                        R.drawable.ic_find_in_page_black_24dp, R.string.find_in_page));
                 add(new ButtonInfo(R.id.desktop_view, R.drawable.ic_desktop_black_24dp,
                         R.string.desktop_view));
             }
